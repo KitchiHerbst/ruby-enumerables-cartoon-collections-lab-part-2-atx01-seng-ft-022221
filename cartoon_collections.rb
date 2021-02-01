83c6b1babf0e9507b67836099b9e975cda7735f0
Triston Herbst <tristonherbst97@gmail.com>
2021-02-01 19:11:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,6 +19,7 @@ def long_planeteer_calls(planeteer_calls)
   # Return the boolean result of this check
   planeteer_calls.map do |element| 
     element = element.capitalize + "!"
+  end
 end
 
 def find_valid_calls(planeteer_calls)
